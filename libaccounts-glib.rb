3e29df7e55e85b740e10181715a1b9398ea7a5e3
Spotlight <spotlight@joscomputing.space>
2018-07-25 16:36:03 -0500

    libaccounts-glib

---
+++ libaccounts-glib.rb
@@ -0,0 +1,41 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class LibaccountsGlib < Formula
+  desc ""
+  homepage ""
+  url "https://gitlab.com/accounts-sso/libaccounts-glib/-/archive/VERSION_1.23/libaccounts-glib-VERSION_1.23.tar.gz"
+  sha256 "80faa991b2a603104f64edfa861c729b7c47a071b998bc1c034b7ec67a221c47"
+  depends_on "pkg-config" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "gtk-doc" => :build
+  depends_on "glib"
+  depends_on "ligsignon-glib"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./autogen.sh"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libaccounts-glib-VERSION`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
