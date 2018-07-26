d1f13c17199ef0ef601775bee4b53eab98f8875b
Spotlight <spotlight@joscomputing.space>
2018-07-25 20:35:23 -0500

    libmediaart

---
+++ libmediaart.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Libmediaart < Formula
+  desc ""
+  homepage ""
+  url "https://download.gnome.org/sources/libmediaart/1.9/libmediaart-1.9.4.tar.xz"
+  sha256 "a57be017257e4815389afe4f58fdacb6a50e74fd185452b23a652ee56b04813d"
+  depends_on "pkg-config" => :build
+  depends_on "glib"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test libmediaart`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
