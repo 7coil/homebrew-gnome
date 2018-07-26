5386f1f9a5a53b3b0c20f153b94f528dbbbf24f9
Spotlight <spotlight@joscomputing.space>
2018-07-26 15:08:20 -0500

    gnome-music

---
+++ gnome-music.rb
@@ -0,0 +1,42 @@
+class GnomeMusic < Formula
+  desc ""
+  homepage ""
+  url "https://download.gnome.org/sources/gnome-music/3.29/gnome-music-3.29.2.tar.xz"
+  sha256 "29c8277e76fa37a711a8d616705fcf20830cd2fa30b97507251b9a7f151bd36c"
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "itstool" => :build
+  depends_on "glib"
+  depends_on "gtk+3"
+  depends_on "gobject-introspection"
+  depends_on "libdazzle"
+  depends_on "libmediaart"
+  depends_on "libsoup"
+  depends_on "tracker"
+  depends_on "spotlightishere/gnome/pygobject3"
+  depends_on "grilo"
+
+  def install
+    # We're doing these both manually afterwards.
+    inreplace "meson.build" do |s|
+      s.gsub! "meson.add_postconf_script('meson_post_conf.py')", ""
+      s.gsub! "meson.add_install_script('meson_post_install.py')", ""
+    end
+
+    mkdir "build" do
+      system "meson", "--prefix=#{prefix}", ".."
+      system "ninja"
+      system "ninja", "install"
+    end
+  end
+
+  def post_install
+      system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+      system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+  end
+
+  test do
+    system "false"
+  end
+end
